4f8364b8201026b070c413d1146fe7e1f31a4760
Colton Shaheen <39281688+coltonshaheen@users.noreply.github.com>
2018-11-10 16:01:19 -0500

    Add files via upload.

---
+++ casear_cipher.rb
@@ -0,0 +1,21 @@
+﻿ASCII_UPPER = 65
+ASCII_LOWER = 97
+
+def caesar_cipher(basic_phrase, shift_factor = 1)
+  basic_chars = basic_phrase.chars
+  encoded_chars = []
+
+  basic_chars.each do |character|
+    char_ascii = character.ord
+
+    if char_ascii.between?(ASCII_UPPER, 90)
+      char_ascii = (char_ascii - ASCII_UPPER + shift_factor) % 26 + ASCII_UPPER
+    elsif char_ascii.between?(ASCII_LOWER, 122)
+      char_ascii = ((char_ascii - ASCII_LOWER + shift_factor) % 26) + ASCII_LOWER
+    end
+    
+    encoded_chars << char_ascii.chr
+  end
+
+  encoded_chars.join('')
+end
